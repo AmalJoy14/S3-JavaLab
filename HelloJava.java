4b737b0314e647cf5ad370cd9641d519b790ea9c
AmalJoy14 <145443570+AmalJoy14@users.noreply.github.com>
2023-09-19 15:05:27 +0530

    My first java program

---
+++ HelloJava.java
@@ -0,0 +1,25 @@
+/**************************************************************
+* File        :   HelloJava.java
+* Description :   My First Java Program
+* Author      :   Amal Joy
+* Date        :   19-09-2023
+***************************************************************/
+
+
+public class HelloJava{
+	public static void main(String [] args) {
+		System.out.println("Hello");
+		System.out.print("Java/n");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
