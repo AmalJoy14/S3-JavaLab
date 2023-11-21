ef75d704979deaf9e735ab3bdf101f0b35f3e096
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2023-11-21 16:28:44 +0530

    Add files via upload

--- FileCopy.java
+++ FileCopy.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 public class FileCopy {
@@ -7,7 +8,7 @@ public class FileCopy {
 		FileWriter writer=null;
 		FileReader reader =null;
 		try {
-			reader=new FileReader("input.txt");
+			reader=new FileReader("nput.txt");
 			int size=reader.read(data);
 			System.out.println(data);
 			System.out.println("Size of data:"+size);
@@ -18,7 +19,11 @@ public class FileCopy {
 			writer.flush();
 			writer.close();
 		}
+		catch(FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
 		catch(IOException e) {
+			System.out.println("ERROR");
 			e.printStackTrace();
 		}
 	}
