d46a7773b52f132a14cad50a8e37371faf44c29e
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2023-12-03 18:01:00 +0530

    Update MultiplicatioTableThread.java

--- MultiplicatioTableThread.java
+++ MultiplicatioTableThread.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class MultipicationTable{
+class MultiplicationTable{
 	synchronized void multiplication(int num) {
 		for (int i=1;i<=10;i++) {
 			System.out.println(num+" X "+i+" = "+num*i);
@@ -9,9 +9,9 @@ class MultipicationTable{
 }
 
 class Thread1 extends Thread{
-	MultipicationTable m;
+	MultiplicationTable m;
 	int digit;
-	Thread1(MultipicationTable m,int digit){
+	Thread1(MultiplicationTable m,int digit){
 		this.m=m;
 		this.digit=digit;
 	}
@@ -21,9 +21,9 @@ class Thread1 extends Thread{
 }
 
 class Thread2 extends Thread{
-	MultipicationTable m;
+	MultiplicationTable m;
 	int digit;
-	Thread2(MultipicationTable m,int digit){
+	Thread2(MultiplicationTable m,int digit){
 		this.m=m;
 		this.digit=digit;
 	}
@@ -33,9 +33,9 @@ class Thread2 extends Thread{
 }
 
 class Thread3 extends Thread{
-	MultipicationTable m;
+	MultiplicationTable m;
 	int digit;
-	Thread3(MultipicationTable m,int digit){
+	Thread3(MultiplicationTable m,int digit){
 		this.m=m;
 		this.digit=digit;
 	}
@@ -47,24 +47,24 @@ class Thread3 extends Thread{
 public class MultiplicatioTableThread {
     public static void main(String [] args) {
     	Scanner sc=new Scanner(System.in);
-    	MultipicationTable mtable=new MultipicationTable();
+    	MultiplicationTable mtable=new MultiplicationTable();
     	int number;
     	
     	System.out.println("Enter the Table that you want to run by thread1:");
-		number =sc.nextInt();
-		Thread1 thread1=new Thread1(mtable,number);
+	number =sc.nextInt();
+	Thread1 thread1=new Thread1(mtable,number);
+	
+	System.out.println("Enter the Table that you want to run by thread2:");   
+	number =sc.nextInt();
+	Thread2 thread2=new Thread2(mtable,number);
 		
-		System.out.println("Enter the Table that you want to run by thread2:");
-		number =sc.nextInt();
-		Thread2 thread2=new Thread2(mtable,number);
+	System.out.println("Enter the Table that you want to run by thread3:");
+	number =sc.nextInt();
+	Thread3 thread3=new Thread3(mtable,number);
 		
-		System.out.println("Enter the Table that you want to run by thread3:");
-		number =sc.nextInt();
-		Thread3 thread3=new Thread3(mtable,number);
-		
-		sc.close();
-		thread1.start();
-		thread2.start();
-		thread3.start();		
+	sc.close();
+	thread1.start();
+	thread2.start();
+	thread3.start();		
     }
 }
